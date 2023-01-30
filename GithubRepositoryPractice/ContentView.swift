7961d50e050e615044bba161b32f43e8159126c8
喜多村海人 <kitamurakaito@kitamurakaijinnoMacBook-Pro.local>
2023-01-30 18:39:10 +0900

    first commit

---
+++ GithubRepositoryPractice/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GithubRepositoryPractice
+//
+//  Created by 喜多村海人 on 2023/01/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GithubRepositoryPractice/GithubRepositoryPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubRepositoryPracticeApp.swift
+//  GithubRepositoryPractice
+//
+//  Created by 喜多村海人 on 2023/01/30.
+//
+
+import SwiftUI
+
+@main
+struct GithubRepositoryPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
